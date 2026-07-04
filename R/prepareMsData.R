39d34a37b587e51aea9d3cfab21370a79938a7f2
catalamarti <marticatalasabate@gmail.com>
2026-07-04 19:14:14 +0100

    Update prepareMsData.R

--- R/prepareMsData.R
+++ R/prepareMsData.R
@@ -1,21 +1,80 @@
 
-prepareMsData <- function(events, tmat) {
+prepareMultiStateData <- function(cohort,
+                                  tmat,
+                                  eventDate = "cohort_start_date",
+                                  indexCohortId = NULL,
+                                  censorDate = NULL,
+                                  stateHeriarchy = character(),
+                                  stateStep = 0.01,
+                                  keepExtraColumns = TRUE) {
+  # initial validations
+  cohort <- omopgenerics::validateCohortArgument(cohort)
+  omopgenerics::validateColumn(eventDate, cohort, type = "date")
+  indexCohortId <- omopgenerics::validateCohortIdArgument({{indexCohortId}}, cohort)
+  omopgenerics::validateColumn(censorDate, cohort, type = "date", null = TRUE)
+  omopgenerics::assertLogical(keepExtraColumns)
+
   # get transitions
   transitions <- tmat |>
     as.data.frame() |>
     tibble::rownames_to_column("from") |>
     tidyr::pivot_longer(!"from", names_to = "to", values_to = "trans") |>
     dplyr::filter(!is.na(.data$trans))
-  notFinal <- transitions |>
-    dplyr::distinct(.data$from) |>
-    dplyr::pull()
+
+  # get not final states
+  notFinal <- unique(transitions$from)
+
+  # states
+  states <- unique(c(notFinal, unique(transitions$to)))
+
+  # check names
+  set <- omopgenerics::settings(cohort)
+  notPresent <- states[!states %in% set$cohort_name]
+  if (length(notPresent) > 0) {
+    cli::cli_abort(c(x = "The following states are not identified in the cohort: {.var {notPresent}}."))
+  }
+
+  # initial states
+  initialStates <- set$cohort_name[set$cohort_definition_id %in% indexCohortId]
+
+  # prepare censor date
+  cohort <- cohort |>
+    PatientProfiles::addCohortName() |>
+    dplyr::filter(.data$cohort_name %in% .env$states) |>
+    PatientProfiles::addFutureObservationQuery(
+      futureObservationType = "date",
+      futureObservationName = "censor_date"
+    )
+  if (!is.null(censorDate)) {
+    cohort <- cohort |>
+      dplyr::mutate(
+        "censor_date" = dplyr::coalesce(.data[[censorDate]], .data$censor_date)
+      )
+  }
+
+  # get states
+  states <- cohort |>
+    dplyr::select(
+      "state" = "cohort_name",
+      "subject_id",
+      "date_event" = dplyr::all_of(indexDate),
+      "censor_date"
+    ) |>
+    dplyr:collect()
 
   # initialise: each subject starts at their first state
-  current <- events |>
+  current <- states |>
+    dplyr::filter(.data$state %in% .env$initialStates) |>
     dplyr::group_by(.data$subject_id) |>
-    dplyr::filter(.data$time_event == min(.data$time_event, na.rm = TRUE)) |>
+    dplyr::filter(.data$date_event == min(.data$date_event, na.rm = TRUE)) |>
     dplyr::ungroup() |>
-    dplyr::select("subject_id", "state", Tstart = "time_event", "censor_time")
+    dplyr::select("subject_id", "state", "date_event")
+
+  # check individuals with more than one initial state
+  multiple <- current |>
+
+
+  # keep only records after initial state
 
   msdata <- list()
   i <- 1L
@@ -69,4 +128,29 @@ prepareMsData <- function(events, tmat) {
   }
 
   dplyr::bind_rows(msdata)
+}
+
+checkMultiple <- function(states, iteration = 0) {
+  multiple <- current |>
+    dplyr::group_by(.data$subject_id) |>
+    dplyr::summarise(
+      n_state = dplyr::n_distinct(.data$state),
+      .groups = "drop"
+    ) |>
+    dplyr::filter(.data$n_state > 1) |>
+    dplyr::tally() |>
+    dplyr::pull() |>
+    as.numeric()
+  if (multiple > 0) {
+    if (iteration == 0) {
+      ini <- "initial "
+      end <- ""
+    } else {
+      ini <- ""
+      end <- paste0(" after transition ", iteration)
+    }
+    message <- paste0("Individuals have multiple ", ini, "states", end, ".")
+    cli::cli_abort(c(x = message, i = "Individuals are not allowed to be in more than one state at the same time."))
+  }
+  invisible()
 }
