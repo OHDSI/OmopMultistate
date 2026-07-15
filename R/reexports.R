ba6ef87609ebcfc861977cf34d66c75307f35b03
catalamarti <marticatalasabate@gmail.com>
2026-07-15 01:18:47 +0100

    re exports

--- R/reexports.R
+++ R/reexports.R
@@ -2,3 +2,27 @@
 #' @importFrom mstate transMat
 #' @export
 mstate::transMat
+
+#' @importFrom omopgenerics tidy
+#' @export
+omopgenerics::tidy
+
+#' @importFrom omopgenerics filterGroup
+#' @export
+omopgenerics::filterGroup
+
+#' @importFrom omopgenerics filterStrata
+#' @export
+omopgenerics::filterStrata
+
+#' @importFrom omopgenerics filterAdditional
+#' @export
+omopgenerics::filterAdditional
+
+#' @importFrom omopgenerics filterSettings
+#' @export
+omopgenerics::filterSettings
+
+#' @importFrom omopgenerics filterResult
+#' @export
+omopgenerics::filterResult
